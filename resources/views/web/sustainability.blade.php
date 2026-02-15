3ac77df8e6444d6b14d2ec6055391cb780a549db
arham <arhamhatim5@gmail.com>
2026-02-15 05:48:18 +0500

    feat: Add new sustainability page with basic layout and breadcrumb.

--- resources/views/web/sustainability.blade.php
+++ resources/views/web/sustainability.blade.php
@@ -3,4 +3,23 @@
 @section('meta_description', 'Discover ICS Group’s commitment to sustainability through responsible operations, environmental stewardship, and long-term value creation for communities, partners, and stakeholders.')
 
 @section('content')
+
+    <!-- Start Breadcrumb 
+            ============================================= -->
+    <div class="breadcrumb-area text-center shadow dark text-light bg-cover"
+        style="background-image: url({{ asset('web_assets/img/2440x1578.png') }});">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 offset-lg-2">
+                    <h1>Sustainability</h1>
+                    <ul class="breadcrumb">
+                        <li><a href="#"><i class="fas fa-home"></i> Home</a></li>
+                        <li class="active">Sustainability</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Breadcrumb -->
+
 @endsection
